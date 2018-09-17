fb601f5c9821213349edad47d3f3ca2a384f313f
Charles Clemens <cclemensjr@gmail.com>
2018-09-17 14:36:27 -0700

    Completed random customer generator and called in each location object

--- js/app.js
+++ js/app.js
@@ -1,40 +1,60 @@
 //DONE - 1. Create objects for each location
 //DONE - 2. Add minHourlyCustomers
 //DONE - 3. Add maxHourlyCustomers
-//4. Add avgCookiesPerSale
+//DONE - 4. Add avgCookiesPerSale
 //5. Create a randCustPerHour method that generates a random number of cust/hr
 //6. Calculate and store the simulated amount of cookies purchased for each hour at each location using avgCookiesPerSale and randCustPerHour
 //7. Store results of calculation in an array
 //8. Display values of each array as an unordered list in the browser
 //9. Calculate the sum of hourly totals and display in browser.
-var firstAndPike = {
+var firstAndPike = 
+{
     minHourlyCustomers: 23,
     maxHourlyCustomers: 65, 
     avgCookiesPerSale : 6.3,
+
+    randCustPerHour   : getCustPerHour,
 }
 
-var seaTacAirport = {
+var seaTacAirport = 
+{
     minHourlyCustomers: 3,
     maxHourlyCustomers: 24,
     avgCookiesPerSale : 1.2,
+
+    randCustPerHour   : getCustPerHour,
 }
 
-var seattleCenter = {
+var seattleCenter = 
+{
     minHourlyCustomers: 11,
     maxHourlyCustomers: 38,
     avgCookiesPerSale : 3.7,
+
+    randCustPerHour   : getCustPerHour,
 }
 
-var capitolHill = {
+var capitolHill = 
+{
     minHourlyCustomers: 20,
     maxHourlyCustomers: 38,
     avgCookiesPerSale : 2.3,
+
+    randCustPerHour   : getCustPerHour,
 }
 
-var alki = {
+var alki = 
+{
     minHourlyCustomers: 2,
     maxHourlyCustomers: 16,
     avgCookiesPerSale : 4.6,
+
+    randCustPerHour   : getCustPerHour,
 }
 
-console.log(firstAndPike.avgCookiesPerSale);
+function getCustPerHour()
+{
+    return Math.random() * 
+          (this.maxHourlyCustomers - this.minHourlyCustomers) +      this.minHourlyCustomers;
+}
+console.log(seaTacAirport.randCustPerHour());
