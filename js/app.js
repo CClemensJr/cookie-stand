6f94e1bb8313138d18957dfe49e1216261f0cd92
Charles Clemens <cclemensjr@gmail.com>
2018-09-17 13:47:44 -0700

    Created object literals for each location

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
-//As a user, I want a webpage that displays individual store data for my Salmon Cookie Shops, so that I can be informed about how to run my business
+//1. Create objects for each location
+var firstAndPike = {
+    
+}
 
-//As a developer, I want to represent the store data in a list format on the webpage, so my client can view the information
+var seaTacAirport = {
 
-//As a developer, I want to use object-oriented programming to build this site, so that the site will be more effective and the code will be easier to read and understand
+}
 
-//DONE - New repository properly set up with a license and README, and cloned to local machine
-//Working on a non-master branch, with regular commit history
-//Good use of Object Literals (no constructors allowed today); one for each store model; properties/values and methods are correctly constructed and given meaningful names
-//Main page meets requirements of the problem domain
-//Use template literals in your JS logic to render the stores as lists on the sales page
+var seattleCenter = {
+
+}
+
+var capitolHill = {
+
+}
+
+var alki = {
+
+}
