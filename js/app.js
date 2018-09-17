67c490aa4d289d7a8ad6f5575db6d3c599dae176
Charles Clemens <cclemensjr@gmail.com>
2018-09-17 13:56:14 -0700

    Added max hourly customers to each location object

--- js/app.js
+++ js/app.js
@@ -1,24 +1,30 @@
 //DONE - 1. Create objects for each location
-//2. 
+//DONE - 2. Add minHourlyCustomers
+//3. Add maxHourlyCustomers
 var firstAndPike = {
-    minHourlyCustomers: 23 
+    minHourlyCustomers: 23,
+    maxHourlyCustomers: 65, 
     
 }
 
 var seaTacAirport = {
-    minHourlyCustomers: 3
+    minHourlyCustomers: 3,
+    maxHourlyCustomers: 24,
 }
 
 var seattleCenter = {
-    minHourlyCustomers: 11
+    minHourlyCustomers: 11,
+    maxHourlyCustomers: 38,
 }
 
 var capitolHill = {
-    minHourlyCustomers: 20
+    minHourlyCustomers: 20,
+    maxHourlyCustomers: 38,
 }
 
 var alki = {
-    minHourlyCustomers: 2
+    minHourlyCustomers: 2,
+    maxHourlyCustomers: 16,
 }
 
-console.log(firstAndPike.minHourlyCustomers);
+console.log(firstAndPike.maxHourlyCustomers);
