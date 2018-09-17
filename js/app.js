6897eaf3534aa9066a62d4b372e3ce1ed1ac0a35
Charles Clemens <cclemensjr@gmail.com>
2018-09-17 13:53:02 -0700

    Added min number of customers per hour to each location object

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
-//1. Create objects for each location
+//DONE - 1. Create objects for each location
+//2. 
 var firstAndPike = {
+    minHourlyCustomers: 23 
     
 }
 
 var seaTacAirport = {
-
+    minHourlyCustomers: 3
 }
 
 var seattleCenter = {
-
+    minHourlyCustomers: 11
 }
 
 var capitolHill = {
-
+    minHourlyCustomers: 20
 }
 
 var alki = {
-
+    minHourlyCustomers: 2
 }
+
+console.log(firstAndPike.minHourlyCustomers);
